8adeb7e7b26d30e89b432bc5df596d215f6f4672
zhar1f09 <zharif0611@gmail.com>
2026-01-07 22:22:20 +0800

    Update imageCategories.js

--- imageCategories.js
+++ imageCategories.js
@@ -1,1 +1,51 @@
+const categories = {
 
+  Modeling: [
+    {
+      title: "Weapons",
+      images: [
+        "Images/Golden Sword.png",
+        "Images/Rainbow Knife.png",
+        "Images/Van.png"
+      ]
+    },
+    {
+      title: "Vehicles",
+      images: [
+        "Images/Van.png",
+        "Images/Golden Sword.png"
+      ]
+    }
+  ],
+
+  Building: [
+    {
+      title: "Structures",
+      images: [
+        "Images/Fantasy House.png",
+        "Images/Concert Stadium.png"
+      ]
+    }
+  ],
+
+  Clothing: [
+    {
+      title: "Jerseys",
+      images: [
+        "Images/Shizuku Jersey 2D.png",
+        "Images/GB Jersey 2D.png"
+      ]
+    }
+  ],
+
+  Server: [
+    {
+      title: "Discord Servers",
+      images: [
+        "Images/Discord Tech Server.png",
+        "Images/Community Shop Server.png"
+      ]
+    }
+  ]
+
+};
