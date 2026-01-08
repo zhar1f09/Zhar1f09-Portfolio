92673806d572e8ae52a359cfbfa7e217df77efae
zhar1f09 <zharif0611@gmail.com>
2026-01-08 19:43:18 +0800

    Update imageCategories.js

--- imageCategories.js
+++ imageCategories.js
@@ -1,80 +1,8 @@
-const imageCategories = {
-  modeling: [
-    {
-      title: "Modeling 1",
-      images: ["Images/Modeling/Hat.png"]
-    },
-    {
-      title: "Modeling 2",
-      images: ["Images/Modeling/MiniVan.png"]
-    },
-    {
-      title: "Modeling 3",
-      images: ["Images/Modeling/LowPolyCar.png"]
-    },
-    {
-      title: "Modeling 4",
-      images: ["Images/Modeling/WoodenHammer.png"]
-    },
-    {
-      title: "Modeling 5",
-      images: ["Images/Modeling/Knife.png"]
-    }
-  ],
-
-  building: [
-    {
-      title: "Building 1",
-      images: ["Images/Building/bunker.png"]
-    },
-    {
-      title: "Building 2",
-      images: ["Images/Building/Concert Stadium.png"]
-    },
-    {
-      title: "Building 3",
-      images: ["Images/Building/Fantasy House.png"]
-    },
-    {
-      title: "Building 4",
-      images: ["Images/Building/HDB Filler Block.png"]
-    }
-  ],
-
-  clothing: [
-    {
-      title: "Clothing 1",
-      images: ["Images/Clothing/GB Jersey 2D.png"]
-    },
-    {
-      title: "Clothing 2",
-      images: ["Images/Clothing/Shizuku Jersey 2D.png"]
-    },
-    {
-      title: "Clothing 3",
-      images: ["Images/Clothing/Shizuku Jersey Black 2D.png"]
-    }
-  ],
-
-  server: [
-    {
-      title: "Server 1",
-      images: ["Images/Server/Server1.png"]
-    },
-    {
-      title: "Server 2",
-      images: ["Images/Server/Server2.png"]
-    }
-  ],
-
-  wip: [
-    {
-      title: "W.I.P 1",
-      images: ["Images/WIP/WIP1.png"]
-    },
-    {
-      title: "W.I.P 2",
-      images: ["Images/WIP/WIP2.png"]
-    }
-  ]
+// imageCategories.js
+const portfolio = {
+  modeling: ["model1.png","model2.png","model3.png"],
+  building: ["building1.png","building2.png","building3.png"],
+  clothing: ["clothing1.png","clothing2.png","clothing3.png"],
+  server: ["server1.png","server2.png","server3.png"],
+  wip: ["wip1.png","wip2.png","wip3.png"]
 };
