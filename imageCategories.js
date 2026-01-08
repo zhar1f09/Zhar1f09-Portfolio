84f6809247e26b7c55b16dd716cb2f45002e512c
zhar1f09 <zharif0611@gmail.com>
2026-01-08 15:02:04 +0800

    Update imageCategories.js

--- imageCategories.js
+++ imageCategories.js
@@ -1,66 +1,31 @@
 const imageCategories = {
   modeling: [
-    {
-      title: "Golden Sword",
-      images: [
-        "Images/Modeling/GoldenSword1.png",
-        "Images/Modeling/GoldenSword2.png",
-        "Images/Modeling/GoldenSword3.png"
-      ]
-    },
-    {
-      title: "Van",
-      images: [
-        "Images/Modeling/Van1.png",
-        "Images/Modeling/Van2.png"
-      ]
-    }
+    "Images/Modeling/GoldenSword1.png",
+    "Images/Modeling/GoldenSword2.png",
+    "Images/Modeling/GoldenSword3.png",
+    "Images/Modeling/Van1.png",
+    "Images/Modeling/Van2.png"
   ],
 
   building: [
-    {
-      title: "Concert Stadium",
-      images: [
-        "Images/Building/Stadium1.png",
-        "Images/Building/Stadium2.png"
-      ]
-    },
-    {
-      title: "Fantasy House",
-      images: [
-        "Images/Building/House1.png",
-        "Images/Building/House2.png"
-      ]
-    }
+    "Images/Building/Stadium1.png",
+    "Images/Building/Stadium2.png",
+    "Images/Building/House1.png",
+    "Images/Building/House2.png"
   ],
 
- clothing: [
-    {
-      title: "Shizuku Jersey",
-      images: [
-        "Images/Clothing/Jersey1.png",
-        "Images/Clothing/Jersey2.png"
-      ]
-    }
+  clothing: [
+    "Images/Clothing/Jersey1.png",
+    "Images/Clothing/Jersey2.png"
   ],
 
   server: [
-    {
-      title: "Tech Discord Server",
-      images: [
-        "Images/Server/Server1.png",
-        "Images/Server/Server2.png"
-      ]
-    }
+    "Images/Server/Server1.png",
+    "Images/Server/Server2.png"
   ],
 
   wip: [
-    {
-      title: "Upcoming Project",
-      images: [
-        "Images/WIP/WIP1.png",
-        "Images/WIP/WIP2.png"
-      ]
-    }
+    "Images/WIP/WIP1.png",
+    "Images/WIP/WIP2.png"
   ]
 };
