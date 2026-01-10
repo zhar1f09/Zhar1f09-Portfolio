19bd60615e741b0719f54cab9c01001237114064
zhar1f09 <zharif0611@gmail.com>
2026-01-10 12:11:20 +0800

    Update imageCategories.js

--- imageCategories.js
+++ imageCategories.js
@@ -1,7 +1,9 @@
+// imageCategories.js
+
 const portfolio = {
-  modeling: ["model1.png","model2.png","model3.png"],
-  building: ["building1.png","building2.png","building3.png"],
-  clothing: ["clothing1.png","clothing2.png","clothing3.png"],
-  server: ["server1.png","server2.png","server3.png"],
-  wip: ["wip1.png","wip2.png","wip3.png"]
+  modeling: ["model1.png", "model2.png", "model3.png"],
+  building: ["building1.png", "building2.png", "building3.png"],
+  clothing: ["clothing1.png", "clothing2.png", "clothing3.png"],
+  server: ["server1.png", "server2.png", "server3.png"],
+  wip: ["wip1.png", "wip2.png", "wip3.png"]
 };
