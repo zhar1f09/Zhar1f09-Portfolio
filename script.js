f1fb3bdc8bbb730be29acbdf7f829a96d9418d94
zhar1f09 <zharif0611@gmail.com>
2026-01-10 12:35:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,16 @@ const buttons = document.querySelectorAll(".filters button");
 
 function renderCategory(category) {
   projectSection.innerHTML = "";
-
-  if (category === "all") {
-    // Show everything
-    Object.keys(portfolio).forEach(cat => {
-      portfolio[cat].forEach(fileName => {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `<img src="Images/${cat}/${fileName}" alt="${cat}">`;
-        projectSection.appendChild(card);
-      });
-    });
-  } else {
-    // Show only selected category
-    portfolio[category].forEach(fileName => {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `<img src="Images/${category}/${fileName}" alt="${category}">`;
-      projectSection.appendChild(card);
-    });
-  }
+  portfolio[category].forEach(fileName => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `<img src="Images/${category}/${fileName}" alt="${category}">`;
+    projectSection.appendChild(card);
+  });
 }
 
-// Default load: show all
-renderCategory("all");
+// Default load: show modeling
+renderCategory("modeling");
 
 // Filter buttons
 buttons.forEach(btn => {
